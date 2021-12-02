15e07a2996e7bfdfaf14ea198d145a285a9ba457
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2021-12-02 19:37:35 +0700

    Add files via upload

---
+++ Aritmatika(2).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    int y;
+    cin >> y;
+    
+    cout << x + y;
+    cout << endl;
+    
+    cout << x - y;
+    cout << endl;
+    
+    cout << x * y;
+    cout << endl;
+    
+    return 0;
+}
